8967f9966a223c981c8232e3c917bee7878fe770
ilyasfarkhane-1500 <ilyasilyas1500@gmail.com>
2024-01-23 16:27:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import QuoteBox from "./components/QuoteBox/QuoteBox"
+
+function App() {
+
+
+  return (
+    <div className='templateQuote container d-flex align-items-center justify-content-center'>
+      <QuoteBox/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -0,0 +1,67 @@
+import "./QuoteBox.css"
+import  { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function QuoteBox() {
+
+//   const quotes = [
+//     {id:1,author:"Kevin Kruse",quote:"Life isn’t about getting and having, it’s about giving and being."},
+//     {id:2,author:"Napoleon Hill",quote:"Whatever the mind of man can conceive and believe, it can achieve."},
+//     {id:3,author:"Albert Einstein",quote:"Strive not to be a success, but rather to be of value."}
+//   ];
+
+  const [currentQuote, setCurrentQuote] = useState({
+    id: null,
+    author: "Auteur par défaut",
+    quote: "Cliquez sur le bouton pour obtenir une citation."
+  });
+
+  const getNewQuote = async () => {
+    try {
+      const response = await axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+      const quotes = response.data.quotes;
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const newQuote = quotes[randomIndex];
+      setCurrentQuote(newQuote);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
+  };
+
+  useEffect(() => {
+    
+    getNewQuote();
+  }, []);
+
+
+
+  return (
+    <div id="quote-box" className="quoteBox container m-auto">
+          <div className="top-quote mt-3 row ">
+            <div  className=" text-center row ">
+            <i className="bi bi-quote col-2"></i>
+              <h1 className=" text-center mt-4" id="text" >{currentQuote.quote}</h1>
+            </div>
+              <div id="author" className="authorSection row">
+                  <h5 className="aut mt-4"> - {currentQuote.author}</h5>
+              </div>
+          </div>
+          <div className="botom-quote  row my-3 ">
+          <div className="col  ">
+              <a className="btn btn-primary  twit" href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">
+                  <i className="bi bi-twitter"></i>
+              </a>
+              <div className="btn btn-primary twit  " >tumblt</div>
+          </div>
+         
+       
+          <div className="col d-flex flex-row-reverse ">
+          <div className="btn btn-primary  me-5" id="new-quote" onClick={getNewQuote} >new-quote</div>
+
+          </div>
+              
+          </div>
+        
+    </div>
+  )
+}
